34ec16d4a27a5838b482d831fc51b7c205ad5026
SiuKeung <siu_keung@outlook.com>
2018-07-10 13:23:26 +0800

    提交了practice_6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,32 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, Integer age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String resultStr = super.introduce() + " I am a Teacher. ";
+        if(this.klass != null)
+            resultStr += "I teach Class " + this.klass + ".";
+        else
+            resultStr += "I teach No Class.";
+        return resultStr;
+    }
 }
