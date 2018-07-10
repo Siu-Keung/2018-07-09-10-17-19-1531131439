344d1887f3f0e7561db74e71149eb77722bad936
SiuKeung <siu_keung@outlook.com>
2018-07-10 14:15:52 +0800

    提交了practice_10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,36 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(Integer id, String name, Integer age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+        this.klass.appendMember(this);
+    }
+
+    public Student(String name, Integer age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String resultStr = super.introduce() + " I am a Student.";
+        if(this.klass.isLeader(this))
+            resultStr += " I am Leader of " + this.klass + ".";
+        else
+            resultStr += " I am at " + this.klass + ".";
+
+        return resultStr;
+    }
+
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,63 @@
 package practice10;
 
-public class Teacher {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teacher extends Person {
+    private List<Klass> klasses = new ArrayList<>();
+
+
+    public List<Klass> getClasses(){
+        return this.klasses;
+    }
+
+    public void setClasses(List<Klass> list){
+        this.klasses = list;
+    }
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public Teacher(Integer id, String name, Integer age, List<Klass> klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    public String introduceWith(Student student){
+        String resultStr = super.introduce() + " I am a Teacher.";
+        if(this.isTeaching(student))
+            resultStr += " I teach " + student.getName() + ".";
+        else
+            resultStr += " I don't teach " + student.getName() + ".";
+        return resultStr;
+    }
+
+    public boolean isTeaching(Student student){
+        for (Klass klass : klasses) {
+            if(klass.isIn(student))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String introduce() {
+        String resultStr = super.introduce() + " I am a Teacher." + this.generateClassSpec();
+        return resultStr;
+    }
+
+    private String generateClassSpec(){
+        if(this.klasses.size() == 0)
+            return " I teach No Class.";
+        else{
+            String resultStr = " I teach Class ";
+            for (Klass klass : klasses) {
+                resultStr += klass.getNumber() + ", ";
+            }
+            resultStr = resultStr.substring(0, resultStr.length() - 2);
+            resultStr += ".";
+            return resultStr;
+        }
+    }
 }
