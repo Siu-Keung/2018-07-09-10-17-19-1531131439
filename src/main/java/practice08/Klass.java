e15ed4a0a50bdbc0ff8a88ee6e999d219afcd675
SiuKeung <siu_keung@outlook.com>
2018-07-10 13:46:49 +0800

    提交了practice_8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,55 @@
 package practice08;
 
 public class Klass {
+    private Integer number;
+    private Student leader;
+
+    public boolean isLeader(Student student){
+        return this.leader == student;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Class " + this.number;
+    }
+
+    public Klass() {
+
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return this.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Klass)
+            return this.number.equals(((Klass)obj).getNumber());
+        else
+            return false;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,43 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public Teacher(Integer id, String name, Integer age, Klass klass) {
+
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+
+    public String introduceWith(Student student){
+        String resultStr = super.introduce() + " I am a Teacher.";
+        if(this.klass.equals(student.getKlass()))
+            resultStr += " I teach " + student.getName() + ".";
+        else
+            resultStr += " I don't teach " + student.getName() + ".";
+        return resultStr;
+    }
+
+    @Override
+    public String introduce() {
+        String resultStr = super.introduce() + " I am a Teacher. ";
+        if(this.klass != null)
+            resultStr += "I teach " + this.klass + ".";
+        else
+            resultStr += "I teach No Class.";
+        return resultStr;
+    }
 }
